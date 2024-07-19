0b360907731086824d1cf24fcbc264f21fa9cb3c
mariaalices <154828341+mariaalices@users.noreply.github.com>
2024-07-19 00:03:28 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,21 +2,25 @@ let amigos = [];
 
 function adicionar() {
 
+    //variavel para "pegar" o nome do amigo inserido
     let nomeAmigo = document.getElementById('nome-amigo');
 
+    //caso o usuario não insira nada, impedi-lo que continue o sorteio
     if(nomeAmigo.value == '') {
         alert('Informe o nome do amigo! ');
         return;
     }
 
+    //caso o nome já tenha sido adicionado
     if(amigos.includes(nomeAmigo.value)) {
         alert('Nome já adicionado!');
         nomeAmigo.value = '';
         return;
     }
 
+    //adicionar os nomes a lista de amigos
     let lista = document.getElementById('lista-amigos');
-
+    
     amigos.push(nomeAmigo.value);
     if (lista.textContent == '') {
         lista.textContent = nomeAmigo.value;
@@ -24,20 +28,22 @@ function adicionar() {
         lista.textContent = lista.textContent + ', ' + nomeAmigo.value;
     }
 
+    //ao adicionar o nome, a caixa reseta 
     nomeAmigo.value = '';
 
 }
 
 function sortear() {
 
+    //para inserir no minimo 4 nomes no sorteio
     if(amigos.length < 4) {
         alert('Adicione pelo menos 4 amigos! ');
         return;
     }
 
+    //sorteio
     embaralha(amigos);
     let sorteio = document.getElementById('lista-sorteio');
-
     for(i = 0; i < amigos.length; i++) {
 
         if(i == amigos.length - 1) {
@@ -63,6 +69,8 @@ function embaralha(lista) {
 }
 
 function reiniciar() {
+
+    //reiniciar o amigo secreto
     amigos = [];
     document.getElementById('lista-amigos').innerHTML = '';
     document.getElementById('lista-sorteio').innerHTML = '';
